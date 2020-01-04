a8a093eeca2987067af084174af15525f044ad96
Vineet Maheshwari <maheshwari.vineet@gmail.com>
2020-01-04 11:33:13 +0530

    playing with controls

--- PongGame.py
+++ PongGame.py
@@ -7,12 +7,18 @@ from kivy.vector import Vector
 from kivy.clock import Clock
 from random import randint
 from kivy.core.window import Window
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 import sys
 import PongBall
 import Player
 
 
-
+#PongGame is graphical widget, which is enhanced with our specific functions
+# This looks for similar name in any of the kivy files, to initialize it's layout
+# >>>>> Didn't understand, how come widget is appearing without any parent layout. Is Widget a layout?
+# 
 class PongGame(Widget):
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
@@ -21,6 +27,9 @@ class PongGame(Widget):
     player2_score = ObjectProperty(None)
     player1_score_value = 0
     player2_score_value = 0
+    player1_name_entered = False
+    player2_name_entered = False
+
     timer_running = False
     timer = None
 
@@ -28,6 +37,28 @@ class PongGame(Widget):
         super(PongGame, self).__init__(**kwargs)
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)    
+
+    def askPlayerNames(self):
+        userNamesInput = BoxLayout(orientation = 'vertical')
+
+        userName1Input = BoxLayout(orientation = 'horizontal')
+        l = Label(text='Player 1', size_hint=(1.0, 1.0), halign="right", valign="middle")
+        l.bind(size=l.setter('text_size'))  
+        userName1Input.add_widget(l)
+        userName1Input.add_widget(TextInput(text='Hello World', multiline=False))
+        userNamesInput.add_widget(userName1Input)
+
+        userName2Input = BoxLayout(orientation = 'horizontal', size=(400,50))
+        l = Label(text='Player 2', halign="right", valign="middle")
+        l.bind(size=l.setter('text_size'))  
+        userName2Input.add_widget(l)
+        userName2Input.add_widget(TextInput(text='Hello World', multiline=False))
+        userNamesInput.add_widget(userName2Input)
+        userNamesInput.pos = (self.x/2 ,self.height/2)
+        userNamesInput.size = (400, 80)
+        self.add_widget(userNamesInput)
+        
+        return
 
     def serve_ball(self):
         self.ball.center = self.center
@@ -38,6 +69,8 @@ class PongGame(Widget):
   
     def update(self, dt):
         print("Being called")
+
+
         # call ball.move and other stuff
         self.ball.move()
         # Check if ball has touched the player 1
@@ -89,13 +122,19 @@ class PongGame(Widget):
             self.player2.center_y -= 20
             if(self.player2.y < 0):
                 self.player2.y += 20
+        elif keycode[1] == 'q' or keycode[1] == 'Q':
+            sys.exit()
         else:
-            if(self.timer_running == False):
+            if(self.timer_running == False and self.player1_name_entered == False and self.player2_name_entered == False):
+                self.askPlayerNames()
+            elif(self.timer_running == False):
                 self.timer = Clock.schedule_interval(self.update, 1.0/60.0)
                 self.timer_running = True
 
 
 class PongApp(App):
+
+#Note that build has to return a layout, which in this case is a widget. This can be boxlayout, relative layout etc
     def build(self):
         game = PongGame()
         game.serve_ball()
